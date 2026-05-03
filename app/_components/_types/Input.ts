3a6f013cb67a5bf737324d87c8e0f3817b7dd751
v1g4-404 <uiga2222@gmial.com>
2026-05-03 10:13:55 +0900

    fix: 不要なファイルを削除

--- app/_components/_types/Input.ts
+++
@@ -1,5 +0,0 @@
-export type Input = {
-  name: string
-  email: string
-  password: string
-}
--- app/_libs/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@/app/generated/prisma/client'
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL
-})
-
-export const prisma = new PrismaClient({ adapter })
